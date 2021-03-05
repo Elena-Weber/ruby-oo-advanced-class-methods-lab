260096e0163dcfe76cdab6ed51e2fb66dcf7bc82
Elena-Weber <crochetmiracles@gmail.com>
2021-03-05 13:47:15 -0500

    commit tree 7d24e57d9ed0a29f02351b6e5de352e9443a278d

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,54 @@ class Song
     self.class.all << self
   end
 
+  def self.create #done
+    song = self.new
+    song.save
+    song
+  end
+
+def self.new_by_name(name) #done
+  song = self.new
+  song.name = name
+  song
+end
+
+def self.create_by_name(name) #done
+  song = self.new
+  song.name = name
+  song.save
+  song
+end
+
+def self.find_by_name(name) #done
+  self.all.detect{|song|song.name == name}
+end
+
+def self.find_or_create_by_name(name) #done
+  self.find_by_name(name) || self.create_by_name(name)
+end
+
+def self.alphabetical #done
+  self.all.sort_by{|song|song.name}
+end
+
+def self.new_from_filename(filename)
+  song = self.new
+  song.artist_name = filename.split(" - ")[0]
+  song.name = filename.split(" - ")[1].chomp(".mp3")
+  song
+end
+
+def self.create_from_filename(filename)
+  song = self.new
+  song.artist_name = filename.split(" - ")[0]
+  song.name = filename.split(" - ")[1].chomp(".mp3")
+  @@all << song
+  song
+end
+
+def self.destroy_all #done
+  @@all.clear
+end
+
 end
